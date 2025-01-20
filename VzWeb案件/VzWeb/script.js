7d639528b01ded19f5d1ac80797f757e1af4b85e
HiroshiSano <hiroshi@bftcweb.com>
2025-01-20 16:40:22 +0900

    変更

--- VzWeb案件/VzWeb/script.js
+++
@@ -1,6 +0,0 @@
-$(function () {
-    $('#js-hamburger-menu, .navigation__link').on('click', function () {
-      $('.navigation').slideToggle(500)
-      $('.hamburger-menu').toggleClass('hamburger-menu--open')
-    });
-  });
--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-$(function () {
-    $('#js-hamburger-menu, .navigation__link').on('click', function () {
-      $('.navigation').slideToggle(500)
-      $('.hamburger-menu').toggleClass('hamburger-menu--open')
+
+  //ハンバーガーメニュー
+$(window).on('load', function () {
+  $(function () {
+    $(".hamburger-button").click(function () {
+      $(this).toggleClass("active");
+      $(".nav-sp").toggleClass("active");
     });
   });
+});
+
+/*
+ページが完全に読み込まれる。
+$(window).on('load', function () {
+
+ユーザーがハンバーガーボタン（.hamburger-button）をクリックする。
+$(".hamburger-button").click(function () {
+
+ハンバーガーボタンにactiveクラスが追加され、スタイルが変化する（例えばアニメーション）。
+$(this).toggleClass("active");
+
+.nav-sp（ナビゲーションメニュー）が表示・非表示を切り替える（displayプロパティやtransformなどで制御している）。
+ $(".nav-sp").toggleClass("active");
+*/
